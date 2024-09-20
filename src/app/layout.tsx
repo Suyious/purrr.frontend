addf7b1888f866f99889a75a3c54c7c97f532fac
Suyash K. <suyu162001@gmail.com>
2024-09-20 23:49:37 +0530

    chore: mobile viewport

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,4 @@
-import type { Metadata } from "next";
+import type { Metadata, Viewport } from "next";
 import { Kalnia } from "next/font/google"
 import "./globals.css";
 
@@ -6,6 +6,12 @@ export const metadata: Metadata = {
   title: "Purrr.chat | Connect Now",
   description: "You can now connect with a random stranger online and chat.",
 };
+
+export const viewport: Viewport = {
+  width: 'device-width',
+  initialScale: 1,
+  interactiveWidget: "resizes-content",
+}
 
 const kalnia = Kalnia({ subsets: ['latin'] })
 

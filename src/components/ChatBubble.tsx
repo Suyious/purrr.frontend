64088b1ca362015d5dec2ef380075b96dd924f58
Harsh Narayan Jha <50262541+HarshNarayanJha@users.noreply.github.com>
2024-09-24 19:18:34 +0530

    feature: create ChatBubble component

--- src/components/ChatBubble.tsx
+++ src/components/ChatBubble.tsx
@@ -12,6 +12,7 @@ export default function ChatBubble({ messages, i, partner, readIndex }: ChatBubb
     const message = messages[i];
     return (
       (message.body || message.image) && <div key={i} className="flex flex-col px-4" style={{ alignItems: message.from === "You" ? "end": "start"}}>
+<<<<<<< HEAD
             { (i === 0 || message.from !== messages[i - 1].from) && <h5 className="text-[0.8em] pt-4">{message.from}</h5> }
             <div className="w-fit">
               {message.image && <Image src={decodeURIComponent(message.image)} alt="Image" width="0" height="0" sizes="100vw" className="w-[20em] h-auto max-h-[20em] rounded-tl-xl rounded-tr-xl mt-1"/>}
@@ -24,6 +25,12 @@ export default function ChatBubble({ messages, i, partner, readIndex }: ChatBubb
               }
             </div>
             { i === readIndex && <p className="text-[0.7em] self-end">Read by {partner}</p>}
+=======
+          { (i === 0 || message.from !== messages[i - 1].from) && <h5 className="text-[0.8em] pt-4">{message.from}</h5> }
+          {message.image && <Image src={decodeURIComponent(message.image)} alt="Image" width="0" height="0" sizes="100vw" className="w-[10em] h-auto max-h-[20em]"/>}
+          {message.body && message.body.trim() !== "" && <h3 className="max-w-[20em] break-words">{message.body}</h3>}
+          { i === readIndex && <p className="text-[0.7em] self-end">Read by {partner}</p>}
+>>>>>>> 6106d09 (feature: create ChatBubble component)
       </div>
     )
 }

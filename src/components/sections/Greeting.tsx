ea36c33e9cc32803e47aad224197273fe6a1ef90
Suyash K. <suyu162001@gmail.com>
2024-09-24 13:37:06 +0530

    fix: close #15 Username length

--- src/components/sections/Greeting.tsx
+++ src/components/sections/Greeting.tsx
@@ -16,22 +16,21 @@ export default function Greeting({ onSubmit }: GreetingProps) {
           onSubmit(username);
         }
       }
-    }, [])
+    }, [onSubmit])
 
     function onSubmitPrevent(e: FormEvent) {
         e.preventDefault();
         if(name.current){
-            onSubmit(name.current.value);
+            if(name.current.value.trim().length !== 0) onSubmit(name.current.value);
         }
     }
-
 
     return (
         <section className="w-full h-full flex justify-center items-center">
             <form onSubmit={onSubmitPrevent} className="flex flex-col items-center gap-4 w-full">
                 <label className="flex flex-col items-center text-center w-full">
                     <span className="text-[1.2em] font-[500]">Your Name</span>
-                    <input maxLength={30} ref={name} className="text-[3em] bg-inherit text-inherit font-[400] width-[10em] max-w-[90%] text-center outline-none border-b-2 border-foreground" type="text" placeholder="Pick a Name"/>
+                    <input required maxLength={30} ref={name} className="text-[3em] bg-inherit text-inherit font-[400] width-[10em] max-w-[90%] text-center outline-none border-b-2 border-foreground" type="text" placeholder="Pick a Name"/>
                 </label>
                 <button className="flex font-[500]">Continue to Chat <RightIcon/> </button>
             </form>

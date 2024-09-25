0012d99c1d94b321c7862ef4228b578c34e264dd
Suyash K. <suyu162001@gmail.com>
2024-09-25 19:59:42 +0530

    fix: Fixes and Closes #41 Hanging Event Handlers

--- src/components/sections/Chat.tsx
+++ src/components/sections/Chat.tsx
@@ -6,12 +6,12 @@ import ScrollDown from "@/assets/icons/scrollDown";
 import SmileyIcon from "@/assets/icons/smiley";
 import { Message } from "@/types/messages";
 import Image from "next/image";
-import { ChangeEvent, FormEvent, useCallback, useEffect, useRef, useState } from "react"
+import { ChangeEvent, FormEvent, KeyboardEventHandler, useCallback, useEffect, useRef, useState } from "react"
 
 type ChatProps = {
     partner: string,
     messages: Message[],
-    onMessage: (message: string|null, image: string|null) => void,
+    onMessage: (message: string | null, image: string | null) => void,
     onStop: () => void,
     onReconnect: () => void,
     readIndex: number | null,
@@ -21,22 +21,24 @@ type ChatProps = {
     partnerTyping: boolean,
 }
 
-export default function Chat({ partner, onMessage, messages, onStop, onReconnect, readIndex,
-                                readMessage, typingStart, typingStop, partnerTyping }: ChatProps) {
+export default function Chat({ 
+    partner, onMessage, messages, onStop, onReconnect, readIndex,
+    readMessage, typingStart, typingStop, partnerTyping }: ChatProps
+) {
 
     const message = useRef<HTMLInputElement>(null);
     const fileinput = useRef<HTMLInputElement>(null);
     const chatBottom = useRef<HTMLDivElement>(null);
 
     let timer: number;
-    const typingTimeout = 1000;
-    let typing = false;
+    const TYPINGTIMEOUT = 500;
 
     const [attachment, setAttachment] = useState<string>("");
     const [unread, setUnread] = useState<number>(0);
+    const [typing, setTyping] = useState<boolean>(false);
 
     const scrollToBottom = useCallback(() => {
-        if(!document.hidden) {
+        if (!document.hidden) {
             setUnread(0);
             readMessage(messages.length - 1);
         }
@@ -46,19 +48,19 @@ export default function Chat({ partner, onMessage, messages, onStop, onReconnect
     function isScrolledToBottom(offsetBefore = 60, offsetAfter = 350) {
         const rect = chatBottom.current?.getBoundingClientRect();
         return (rect && rect.bottom >= (window.innerHeight || document.documentElement.clientHeight) - offsetBefore
-                     && rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) + offsetAfter);
+            && rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) + offsetAfter);
     }
 
     function isScrollAvailable(offset = 60) {
         const rect = chatBottom.current?.getBoundingClientRect();
         return (rect && !(rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) - offset))
     }
-    
+
     useEffect(() => {
-        if(messages.length > 0) {
+        if (messages.length > 0) {
             const last_message = messages[messages.length - 1];
-            if(!isScrollAvailable()) {
-                if(!document.hidden) {
+            if (!isScrollAvailable()) {
+                if (!document.hidden) {
                     readMessage(messages.length - 1);
                 } else {
                     setUnread(n => n + 1);
@@ -66,8 +68,8 @@ export default function Chat({ partner, onMessage, messages, onStop, onReconnect
             } else {
                 if (isScrolledToBottom() || last_message.from === "You") {
                     scrollToBottom();
-                    if(document.hidden) setUnread(n => n + 1);
-                } else if(last_message.from !== "You"){
+                    if (document.hidden) setUnread(n => n + 1);
+                } else if (last_message.from !== "You") {
                     setUnread(n => n + 1);
                 }
             }
@@ -75,9 +77,9 @@ export default function Chat({ partner, onMessage, messages, onStop, onReconnect
     }, [messages, readMessage, scrollToBottom]);
 
     const markReadIfViewed = useCallback(() => {
-        if(isScrolledToBottom() || !isScrollAvailable()) {
+        if (isScrolledToBottom() || !isScrollAvailable()) {
             setUnread(0);
-            if(!document.hidden) readMessage(messages.length - 1);
+            if (!document.hidden) readMessage(messages.length - 1);
         }
     }, [messages.length, readMessage])
 
@@ -102,55 +104,56 @@ export default function Chat({ partner, onMessage, messages, onStop, onReconnect
             document.removeEventListener("visibilitychange", markReadIfViewed);
         }
     }, [markReadIfViewed])
-  
-    message.current?.addEventListener('keypress', (e) => {
+
+    const onKeyDown: KeyboardEventHandler<HTMLInputElement> = (e) => {
         if (e.key === "Enter") return;
 
         window.clearTimeout(timer);
         if (!typing) {
-          typingStart();
-          typing = true;
+            typingStart();
+            setTyping(true);
         }
-    });
+    };
 
-    message.current?.addEventListener('keyup', () => {
+    const onKeyUp = () => {
         window.clearTimeout(timer);
         timer = window.setTimeout(() => {
-          typingStop();
-          typing = false;
-        }, typingTimeout)
-    });
-    
+            typingStop();
+            setTyping(false);
+        }, TYPINGTIMEOUT)
+    };
+
     function onSubmit(e: FormEvent) {
         e.preventDefault();
 
-        let msg: string|null = null, img:string|null = null;
+        let msg: string | null = null, img: string | null = null;
 
-        if(message.current){
-            if(message.current.value.trim() !== "") msg = message.current.value;
+        if (message.current) {
+            if (message.current.value.trim() !== "") msg = message.current.value;
             message.current.value = "";
             message.current.focus();
         }
 
-        if(attachment.length !== 0) {
+        if (attachment.length !== 0) {
             img = attachment;
             setAttachment("");
         }
-        if(msg || img) onMessage(msg, img);
+        if (msg || img) onMessage(msg, img);
         typingStop();
-        typing = false;
+        setTyping(false);
     }
 
-    function onFileChange(e: ChangeEvent<HTMLInputElement>) {
-        const file = e.target.files?.[0];
+    function onFileChange(event: ChangeEvent<HTMLInputElement>) {
+        const file = event.target.files?.[0];
         console.log(file);
-        if(file) {
+        if (file) {
             // Array.from(files).forEach(file => {
             const reader = new FileReader();
             reader.onload = (e) => {
                 const base64Image = e.target?.result as string;
                 // setAttachments(attachs => [...attachs, base64Image]);
                 setAttachment(base64Image);
+                event.target.value = "";
             };
             reader.readAsDataURL(file);
             // })
@@ -171,49 +174,49 @@ export default function Chat({ partner, onMessage, messages, onStop, onReconnect
 
             <div className="flex flex-col justify-end w-[1080px] max-w-full min-h-full py-[5em]">
                 {messages.map((message, i) => (
-                    (message.body || message.image) && <div key={i} className="flex flex-col px-4" style={{ alignItems: message.from === "You" ? "end": "start"}}>
-                        { (i === 0 || message.from !== messages[i - 1].from) && <h5 className="text-[0.8em] pt-4">{message.from}</h5> }
-                        {message.image && <Image src={decodeURIComponent(message.image)} alt="Image" width="0" height="0" sizes="100vw" className="w-[10em] h-auto max-h-[20em]"/>}
+                    (message.body || message.image) && <div key={i} className="flex flex-col px-4" style={{ alignItems: message.from === "You" ? "end" : "start" }}>
+                        {(i === 0 || message.from !== messages[i - 1].from) && <h5 className="text-[0.8em] pt-4">{message.from}</h5>}
+                        {message.image && <Image src={decodeURIComponent(message.image)} alt="Image" width="0" height="0" sizes="100vw" className="w-[10em] h-auto max-h-[20em]" />}
                         {message.body && message.body.trim() !== "" && <h3 className="max-w-[20em] break-words">{message.body}</h3>}
-                        { i === readIndex && (message.from === "You" ? <p className="text-[0.7em] self-end">Read by {partner}</p>: <div className="h-[1em]"/>)}
+                        {i === readIndex && (message.from === "You" ? <p className="text-[0.7em] self-end">Read by {partner}</p> : <div className="h-[1em]" />)}
                     </div>
                 ))}
                 <div ref={chatBottom}></div>
             </div>
 
-            { unread > 0 && <button onClick={scrollToBottom} className="fixed bottom-[5em]">
+            {unread > 0 && <button onClick={scrollToBottom} className="fixed bottom-[5em]">
                 <div className="bg-foreground font-[800] font-mono text-background text-[0.8em] w-5 h-5 flex justify-center items-center rounded-[50%] absolute right-0">{unread}</div>
-                <ScrollDown/>
-            </button> }
+                <ScrollDown />
+            </button>}
 
             <header className="fixed top-0 left-0 w-full bg-background font-display">
                 <div className="w-[1080px] max-w-full p-4 m-auto flex justify-between items-end">
                     <div className="">
                         <h4 className="text-[0.8em]">You&apos;re connected to</h4>
-                        <h2>{partner} { partnerTyping && <small><i>Typing...</i></small> }</h2>
+                        <h2>{partner} {partnerTyping && <small className="font-text">Typing...</small>}</h2>
                     </div>
                     <div className="flex gap-2">
-                        <button onClick={onRefresh}><RefreshIcon/></button>
-                        <button onClick={onStop}><ExitIcon/></button>
+                        <button onClick={onRefresh}><RefreshIcon /></button>
+                        <button onClick={onStop}><ExitIcon /></button>
                     </div>
                 </div>
             </header>
 
             <form onSubmit={onSubmit} className="fixed bg-background bottom-[1em] border-[1px] border-foreground p-2 w-[1080px] max-w-[95vw] flex rounded-[50px]">
-                { attachment.length > 0 && <div className="absolute -top-24 border-[1px] border-foreground rounded-lg">
+                {attachment.length > 0 && <div className="absolute -top-24 border-[1px] border-foreground rounded-lg">
                     <button type="button" onClick={clearAttachment} className="bg-foreground font-[800] font-mono text-background text-[0.8em] w-5 h-5 flex justify-center items-center rounded-[50%] absolute -top-2 -right-2">
-                        <CloseIcon width="18" fill="background"/>
+                        <CloseIcon width="18" fill="background" />
                     </button>
                     <Image src={decodeURIComponent(attachment)} alt="Image" width="0" height="0" sizes="100vw" className="w-[5em] h-[5em] rounded-lg object-cover" />
-                </div> }
+                </div>}
                 <button type="button" className="mr-2 w-[30px] h-[30px] flex justify-center items-center rounded-md">
-                    <SmileyIcon/>
+                    <SmileyIcon />
                 </button>
                 <button onClick={() => fileinput.current?.click()} type="button" className="mr-2 w-[25px] h-[25px] flex justify-center items-center rounded-md">
-                    <input accept="image/*" onChange={onFileChange} ref={fileinput} type="file" className="hidden"/>
-                    <AttachmentIcon width="20"/>
+                    <input accept="image/*" onChange={onFileChange} ref={fileinput} type="file" className="hidden" />
+                    <AttachmentIcon width="20" />
                 </button>
-                <input ref={message} autoFocus className="bg-inherit flex-1 outline-none text-base" type="text" placeholder="Send a message"/>
+                <input ref={message} onKeyDown={onKeyDown} onKeyUp={onKeyUp} autoFocus className="bg-inherit flex-1 outline-none text-base" type="text" placeholder="Send a message" />
                 <button className="text-sm px-4 rounded-md" type="submit">Send</button>
             </form>
 

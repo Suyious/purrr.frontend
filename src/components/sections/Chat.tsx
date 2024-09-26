efcea9f01310808e549aa774b7d38c6153e43823
Suyash K. <suyu162001@gmail.com>
2024-09-26 18:43:22 +0530

    chore: move utility functions to utils folder

--- src/components/sections/Chat.tsx
+++ src/components/sections/Chat.tsx
@@ -7,8 +7,9 @@ import ReplyIcon from "@/assets/icons/reply";
 import ScrollDown from "@/assets/icons/scrollDown";
 import SmileyIcon from "@/assets/icons/smiley";
 import { Message } from "@/types/messages";
+import { setEmojiSize, truncate } from "@/utils";
 import Image from "next/image";
-import { ChangeEvent, FormEvent, useCallback, useEffect, useRef, useState } from "react"
+import { ChangeEvent, FormEvent, useCallback, useEffect, useRef, useState } from "react";
 
 type ChatProps = {
     partner: string,
@@ -181,24 +182,6 @@ export default function Chat({
     function replyTo(messageId: number | null) {
         setReplyingTo(messageId);
         message.current?.focus();
-    }
-
-    function setEmojiSize(str: string): string {
-        const emojiRegex: RegExp = new RegExp(/[\uD800-\uDBFF]|[\u2702-\u27B0]|[\uF680-\uF6C0]|[\u24C2-\uF251]/g);
-        const matchedEmojis = str.match(emojiRegex);
-
-        if(matchedEmojis !== null && matchedEmojis.length > 0 && matchedEmojis.join('') === str) {
-            const length = matchedEmojis.length;
-            if(length <= 2) return "4em";
-            if(length <= 4) return "3em";
-            if(length <= 8) return "2em";
-        }
-        return "1em";
-    }
-
-    function truncate(str: string | null, max = 40) {
-        if(str === null) return null;
-        return str.substring(0, max) + (str.length > 40 ? "...": "")
     }
 
     return (

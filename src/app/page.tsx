dc621173e23783bae1ad299f11699b3b808ca1b9
Suyash K. <suyu162001@gmail.com>
2024-10-01 19:23:16 +0530

    feat: move chat to /chat

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,57 +1,30 @@
-"use client"
-import Chat from "@/components/sections/Chat";
-import Greeting from "@/components/sections/Greeting";
-import NotConnected from "@/components/sections/NotConnected";
-import StartChatting from "@/components/sections/StartChatting";
-import WaitingForChat from "@/components/sections/WaitingForChat";
-import { useChatSocket } from "@/hooks/useChatSocket";
+import Footer from "@/components/partials/Footer";
+import Navigation from "@/components/partials/Navigation";
+import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
-
-  const {
-    user,
-    partner,
-    messages,
-    isConnected,
-    isWaiting,
-    readIndex,
-    setUserName,
-    findPartner,
-    sendMessage,
-    readMessage,
-    startTyping,
-    stopTyping,
-    partnerTyping,
-    disconnect,
-  } = useChatSocket();
-
-  let content;
-
-  if (!isConnected) {
-    content = <NotConnected/>;
-  } else {  // if connected to server
-    if (!user) {
-      content = <Greeting onSubmit={setUserName} />;
-    } else { // if connected to server and username is set
-      if (!isWaiting) {
-        if (!partner) { // if user hasn't allowed to find matches
-          content = <StartChatting name={user} onConnect={findPartner} />;
-        } else { // if matches found
-          content = <Chat partner={partner}
-                          onMessage={sendMessage} messages={messages}
-                          onStop={disconnect} onReconnect={findPartner}
-                          readIndex={readIndex} readMessage={readMessage}
-                          startTyping={startTyping} stopTyping={stopTyping} partnerTyping={partnerTyping} />;
-        }
-      } else { // if user is waiting for a partner
-        content = <WaitingForChat />;
-      }
-    }
-  }
-
   return (
-    <main className="w-full h-full font-display">
-      {content}
+    <main className="w-full h-full">
+      <Navigation/>
+      <header className="relative">
+        <div className="relative">
+          <Image className="h-[20em] sm:h-[25em] lg:h-[30em]" src="/landingbg.jpg" width="3596" height="1280" style={{
+            objectFit: "cover",
+            objectPosition: "10%",
+          }} alt="background for the landing page"/>
+          <div className="absolute bottom-0 w-full h-[10em] bg-[linear-gradient(180deg,_rgba(255,_255,_255,_0)_0%,var(--background))]"></div>
+        </div>
+        <div className="w-full pb-16">
+          <div className="max-w-[90vw] m-auto flex flex-col items-center text-center font-display">
+            <h2 className="w-full text-[1.5em] md:text-[3em]">Connect, Chat, and Explore</h2>
+            <h1 className="w-full text-[2.5em] md:text-[4em] font-[600]">Your Conversations, Your Way</h1>
+            <p className="font-text w-[580px] max-w-full m-auto text-[1em] md:text-[1.2em]">Meet new people, share moments, and chat with total freedom. No sign-ups, no data logs, no limits.</p>
+            <Link className="bg-accent text-white font-text font-[700] text-[1em] md:text-[1.4em] px-16 py-4 mt-8 rounded-[50px]" href="/chat">Start Chatting Now</Link>
+          </div>
+        </div>
+      </header>
+      <Footer/>
     </main>
   );
 }
--- src/components/sections/StartChatting.tsx
+++ src/components/sections/StartChatting.tsx
@@ -13,9 +13,9 @@ export default function StartChatting({ name, onConnect }: StartChattingProps) {
 
     return (
         <section className="w-full h-full flex justify-center items-center">
-            <div className="text-center">
+            <div className="text-center max-w-[80vw]">
                 <h2 className="text-[1.4em]">Hi, { name }</h2>
-                <h1 className="text-[3em]">Connect Now to Chat</h1>
+                <h1 className="text-[3em] leading-[1em]">Connect Now to Chat</h1>
                 <button onClick={onConnect} className="mt-4 py-2 px-16 border-2 border-foreground rounded-lg text-[1.1em]">Connect Now</button>
                 <p className="mt-4">You can connect to a random stranger and start chatting now.</p>
             </div>

7e5c8136a7f3682ace82e26bb3e8baf1168391b9
Suyash K. <suyu162001@gmail.com>
2024-10-08 22:41:38 +0530

    chore: Organize ChatDisplay into new component

--- src/components/sections/Chat.tsx
+++ src/components/sections/Chat.tsx
@@ -1,13 +1,10 @@
 import ExitIcon from "@/assets/icons/exit";
-// import MenuIcon from "@/assets/icons/menu";
 import RefreshIcon from "@/assets/icons/refresh";
-import ReplyIcon from "@/assets/icons/reply";
 import ScrollDown from "@/assets/icons/scrollDown";
 import { Message } from "@/types/messages";
-import { setEmojiSize, truncate } from "@/utils";
-import Image from "next/image";
 import { ChangeEvent, FormEvent, KeyboardEventHandler, useCallback, useEffect, useRef, useState } from "react";
 import ChatInput from "../resusable/ChatInput";
+import { ChatDisplay } from "../resusable/ChatDisplay";
 
 type ChatProps = {
     partner: string,
@@ -207,48 +204,7 @@ export default function Chat({
     return (
         <section className={"w-full flex justify-center items-end relative font-text"}>
 
-            <div className="flex flex-col justify-end w-[1080px] max-w-full min-h-full pt-[5em]" style={{
-                paddingBottom: chatHeightOffset + "px",
-            }}>
-                {messages.map((message, i) => (
-                    (message.body || message.image) &&
-                    <div key={i} className="group flex flex-col px-4" style={{
-                        alignItems: message.from === "You" ? "end" : "start"
-                    }}>
-                        {(i === 0 || message.from !== messages[i - 1].from) &&
-                            <h5 className="text-[0.8em] pt-4">{message.from}</h5>}
-                        {message.reply !== null && 
-                            <div className="flex flex-col border-foreground px-2" style={{
-                                alignItems: message.from === "You"? "end": "start",
-                                borderRightWidth: message.from === "You"? "3px": "0",
-                                borderLeftWidth: message.from === "You"? "0": "3px",
-                            }}>
-                                <h3 className="text-[0.7em]">Replying to {messages[message.reply].from}</h3>
-                                <p className="text-[0.8em]">{messages[message.reply].image && "(Attachment)"}{truncate(messages[message.reply].body)}</p>
-                            </div>}
-                        {message.image &&
-                            <div className="flex items-center gap-4" style={{ flexDirection: message.from === "You" ? "row-reverse" : "row" }}>
-                                <Image src={decodeURIComponent(message.image)} alt="Image" width="0" height="0" sizes="100vw" className="w-[10em] h-auto max-h-[20em]" />
-                                <div className="hidden group-hover:flex items-center gap-2">
-                                    <button onClick={() => replyTo(i)}><ReplyIcon width="18" /></button>
-                                    {/* <button><MenuIcon width="20" /></button> */}
-                                </div>
-                            </div>}
-                        {message.body && message.body.trim() !== "" &&
-                            <div className="flex items-center gap-4" style={{ flexDirection: message.from === "You" ? "row-reverse" : "row" }}>
-                                <h3 className="max-w-[70vw] break-words whitespace-pre-line" style={{ fontSize: setEmojiSize(message.body) }}>{message.body}</h3>
-                                {!message.image && <div className="hidden group-hover:flex items-center gap-2">
-                                    <button onClick={() => replyTo(i)}><ReplyIcon width="18" /></button>
-                                    {/* <button><MenuIcon width="20" /></button> */}
-                                </div>}
-                            </div>}
-                        {i === readIndex && (message.from === "You" ?
-                            <p className="text-[0.7em] self-end">Read by {partner}</p> :
-                            <div className="h-[1em]" />)}
-                    </div>
-                ))}
-                <div ref={chatBottom}></div>
-            </div>
+            <ChatDisplay chatBottom={chatBottom} chatHeightOffset={chatHeightOffset} messages={messages} partner={partner} readIndex={readIndex} replyTo={replyTo}/>
 
             {unread > 0 && <button onClick={scrollToBottom} className="fixed bottom-[5em]">
                 <div className="bg-foreground font-[800] font-mono text-background text-[0.8em] w-5 h-5 flex justify-center items-center rounded-[50%] absolute right-0">{unread}</div>

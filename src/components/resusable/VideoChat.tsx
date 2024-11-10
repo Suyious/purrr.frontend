c1b8cee06ff3df76b4237836025eced3898212c4
Harsh Narayan Jha <50262541+HarshNarayanJha@users.noreply.github.com>
2024-11-10 17:17:49 +0530

    ui: rework video chat component and add end call button

--- src/components/resusable/VideoChat.tsx
+++ src/components/resusable/VideoChat.tsx
@@ -1,15 +1,28 @@
-import { WebRTCState } from "@/hooks/useWebRTC";
+import { type WebRTCState } from "@/hooks/useWebRTC";
 
 type VideoChatProps = {
-    webRTCState: WebRTCState
+    webRTCState: WebRTCState,
+    endVideoCall: (localEvent: boolean) => void
 };
 
-export function VideoChat({ webRTCState }: VideoChatProps) {
+export function VideoChat({ webRTCState, endVideoCall }: VideoChatProps) {
     return (
-        <div className="flex flex-row">
-            {
-                webRTCState.localStream && (
+        <div className="flex flex-col items-center">
+            <div className="flex flex-row relative">
+                {webRTCState.remoteStream && (
                     <video
+                        className="rounded-md mx-2 my-3"
+                        autoPlay
+                        playsInline
+                        ref={video => {
+                            if (video) video.srcObject = webRTCState.remoteStream;
+                        }}
+                    />
+                )}
+
+                {webRTCState.localStream && (
+                    <video
+                        className="rounded-md mx-2 my-3 absolute bottom-5 right-5 w-28"
                         autoPlay
                         playsInline
                         muted
@@ -17,21 +30,14 @@ export function VideoChat({ webRTCState }: VideoChatProps) {
                             if (video) video.srcObject = webRTCState.localStream;
                         }}
                     />
-                )
-            }
+                )}
 
-            {
-                webRTCState.remoteStream && (
-                    <video
-                        autoPlay
-                        playsInline
-                        muted
-                        ref={video => {
-                            if (video) video.srcObject = webRTCState.remoteStream;
-                        }}
-                    />
-                )
-            }
+            </div>
+            <button
+                className="px-3 py-2 bg-red-600 hover:bg-red-400 transition-colors rounded-3xl text-white"
+                onClick={() => endVideoCall(true)}>
+                End Call
+            </button>
         </div>
     )
 }
